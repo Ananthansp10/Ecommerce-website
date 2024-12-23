ba9e59543e5e3707f3c6910fbc018879b445c604
Ananthan sp <spananthan920@gmail.com>
2024-12-24 04:11:14 +0530

    mongo url changes

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,12 +1,22 @@
 const mongoose=require('mongoose');
-//const mongoURL='mongodb://13.61.0.254:27017/ecommerce';
-//const mongoURL='mongodb://13.61.0.254:27017/ecommerce';
 const mongoURL='mongodb://127.0.0.1:27017/ecommerce'
 
-mongoose.connect(mongoURL).then(()=>{
-    console.log("database connected")
-}).catch((err)=>{
-    console.log("database not connected",+err)
-})
+// mongoose.connect(mongoURL).then(()=>{
+//     console.log("database connected")
+// }).catch((err)=>{
+//     console.log("database not connected",+err)
+// })
+
+mongoose.connect('mongodb://127.0.0.1:27017/ecommerce', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+      console.error('Database connection error:', err);
+    });
+  
 
 module.exports=mongoose;
